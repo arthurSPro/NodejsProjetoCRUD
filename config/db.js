a17a7171bfb9bf77cd690717f4e55f5273751905
Arthur Reis <arthurds008@gmail.com>
2021-06-19 22:08:52 -0300

    Changed some lines

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://newUser:762556@cluster0.v7ih4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
-}else{
-    module.exports = {mongoURI: "mongodb://localhost/blogapp"}
+if (process.env.NODE_ENV == "production") {
+    module.exports = { mongoURI: "mongodb+srv://newUser:762556@cluster0.v7ih4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" }
+} else {
+    module.exports = { mongoURI: "mongodb://localhost/blogapp" }
 }
